3d92e076f98e32000b63338a4ae71c9564e845a2
Tomislav Dukez <tomdu3@ymail.com>
2025-02-07 06:26:23 +0000

    add auth paths

--- website/auth.py
+++ website/auth.py
@@ -2,4 +2,16 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+@auth.route('/register')
+def register():
+    return "<p>register</p>"
+
 
