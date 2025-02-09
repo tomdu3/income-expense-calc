4b2237b7cba467651b86ef5f7425f8c46788a5c9
Tomislav Dukez <tomdu3@ymail.com>
2025-02-09 13:04:51 +0000

    add params to login view and template

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", text="Testing", username="tom", boolean=False)
 
 
 @auth.route("/logout")
