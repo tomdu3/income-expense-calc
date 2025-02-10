50e01145abb937306748613c9495bfa90bea7ce7
Tomislav Dukez <tomdu3@ymail.com>
2025-02-10 07:39:56 +0000

    add dotenv module for .env file

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = environ.get('SECRET_KEY', '')
+    app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "")
+    print(environ.get("SECRET_KEY", "no key"))
 
-    from .views import  views
+    from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/auth/")
     return app
