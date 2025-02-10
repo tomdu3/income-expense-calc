90be3147f319d8d649f7e26cf84c61e64821e6d5
Tomislav Dukez <tomdu3@ymail.com>
2025-02-10 07:40:26 +0000

    add flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -17,4 +17,29 @@ def logout():
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        email = request.form.get("email").strip()
+        first_name = request.form.get("firstName").strip()
+        last_name = request.form.get("lastName").strip()
+        password = request.form.get("password").strip()
+        confirm_password = request.form.get("confirmPassword").strip()
+        if len(email) < 4:
+            flash(
+                "Email is too short. Must be at least 4 characters.", category="danger"
+            )
+        elif len(first_name) < 2 or len(last_name) < 2:
+            flash(
+                "First name and last name must be at least 2 characters.",
+                category="danger",
+            )
+        elif len(password) < 8:
+            flash("Password must be at least 8 characters.", category="danger")
+        elif password != confirm_password:
+            flash("Passwords do not match.", category="danger")
+        else:
+            # add user to database
+            flash("User created successfully.", category="success")
+
+        return render_template("register.html", email=email, password=password)
+
     return render_template("register.html")
