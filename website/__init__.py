d3f61cfddc414e3e3d33c2995667602058c53719
Tomislav Dukez <tomdu3@ymail.com>
2025-02-28 15:41:34 +0000

    add create database to setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from os import environ
+from os import environ, path
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,7 +13,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "")
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] =\
+        f"sqlite:///{path.join(path.dirname(__file__), DB_NAME)}"  ## overriding default db creation on instance/
 
     db.init_app(app)
 
@@ -22,4 +23,17 @@ def create_app():
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth/")
+
+    from . import models
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    with app.app_context():
+        if not path.exists(path.join(path.dirname(__file__), DB_NAME)):
+            db.create_all()
+            print("Created Database!")
+
