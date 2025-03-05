4f4be32aafa7a87fb355d3dc0452aa16841d2946
Tomislav Dukez <tomdu3@ymail.com>
2025-03-05 14:30:16 +0000

    add login manager

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from os import environ, path
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv()
 
@@ -13,8 +14,9 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = environ.get("SECRET_KEY", "")
-    app.config["SQLALCHEMY_DATABASE_URI"] =\
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"sqlite:///{path.join(path.dirname(__file__), DB_NAME)}"  ## overriding default db creation on instance/
+    )
 
     db.init_app(app)
 
@@ -24,9 +26,17 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth/")
 
-    from . import models
+    from .models import User, Category, Transaction
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
 
@@ -36,4 +46,3 @@ def create_database(app):
         if not path.exists(path.join(path.dirname(__file__), DB_NAME)):
             db.create_all()
             print("Created Database!")
-
